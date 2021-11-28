08250447ab39cf20bac560d7f1241f9ff2e6ac3b
root <anderssom05@gmail.com>
2021-11-28 06:27:21 -0300

    versao 28/11/21

---
+++ src/page/Dashcboard/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Title } from './styles';
+
+const Dashboar: React.FC = () => { 
+    
+    return <Title>Explore Repositórios  no Github</Title>
+
+}
+export default Dashboar;
---
+++ src/router/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {Switch, Route} from 'react-router-dom';
+import Dashboard from '../page/Dashcboard';
+
+import Repository from '../page/Repository';
+
+const Rotas: React.FC = () => (
+
+    <Switch>
+
+        <Route path="/" exact component={Dashboard}   />
+        <Route path="/repository" component={Repository}/>
+
+    </Switch>
+)
+
+export default Rotas
